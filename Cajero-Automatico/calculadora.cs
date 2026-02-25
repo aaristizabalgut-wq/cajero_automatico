43f6c5daf4737db7f1b280faff6b169a3e0b0bd1
Ana <aaristizabalgut@gmail.com>
2026-02-25 15:26:25 -0500

    se agrego una nueva clasee calculadora

---
+++ Cajero-Automatico/calculadora.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cajero_Automatico
+{
+    internal class calculadora
+    {
+    }
+}
--- Cajero-Automatico/obj/Debug/net10.0/Cajero-Automatico.AssemblyInfo.cs
+++ Cajero-Automatico/obj/Debug/net10.0/Cajero-Automatico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cajero-Automatico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f4f185ad1b977414519c8b23e66bdec82d48a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cajero-Automatico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cajero-Automatico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
